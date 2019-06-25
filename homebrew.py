4347c3ea011a053d644f0814e15ab67f3c5bebdb
Patrick Fuller <patrickbfuller@gmail.com>
2019-06-25 11:32:55 -0700

    implement owner_id to playlist extraction and implement artist set retriever

--- homebrew.py
+++ homebrew.py
@@ -11,5 +11,15 @@ def extract_name_and_id(response:dict):
     for playlist in playlist_objects:
         id_code = playlist['id']
         name = playlist['name']
-        result[id_code] = name
+        owner_id = playlist['owner']['id']
+        result[id_code] = {'name': name, 'owner_id': owner_id}
     return result
+
+def get_artists_in_playlist(playlist_data):
+    artists = []
+    track_list = playlist_data['items']
+    for track_data in track_list:
+        artist_data = track_data['track']['artists']
+        for artist in artist_data:
+            artists.append(artist['name'])
+    return set(artists)
