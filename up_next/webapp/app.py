68ec26318917920710c8ee6a8b19ee4ec2126987
Patrick Fuller <patrickbfuller@gmail.com>
2019-07-08 11:26:59 -0700

    start app.py to use map

--- up_next/webapp/app.py
+++ up_next/webapp/app.py
@@ -8,6 +8,7 @@ with open('up_next/webapp/sim_artist_model.pkl', 'rb') as f:
     model = pickle.load(f)
 app = Flask(__name__, static_url_path="")
 
+
 @app.route('/')
 def index():
     """Return the main page."""
@@ -16,7 +17,9 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    """Return a random prediction."""
-    data = request.json
-    prediction = model.predict_proba([data['user_input']])
-    return jsonify({'probability': prediction[0][1]})
+    """Return folium map."""
+    artist = request.json
+    untapped = model.find_similar_artist_venues(artist)
+    m = viz.add_multi_artist_markers(artist, untapped)    
+    # return jsonify({'probability': prediction[0][1]})
+    return m
